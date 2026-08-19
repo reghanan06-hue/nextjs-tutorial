b4973a82f9e0dfe98c80f3dbb516cb2a4a5da997
reghanan06-hue <reghanan06@gmail.com>
2026-08-19 20:20:23 +0100

    Add page not found on  nextjs

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+export default function notfound() {
+  return (
+    <div className="flex flex-col 
+    items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold text-red-700">Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  )
+}
+
