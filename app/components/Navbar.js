5235705656ff5292847443eb7536b3c63b2422b5
reghanan06-hue <reghanan06@gmail.com>
2026-08-21 23:26:10 +0100

    Add style tailwind css of  navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,11 +1,25 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+    const pathname=usePathname();
+    const isActive = (path) => {
+        return pathname === path ? "text-blue-500 font-bold underline" : "text-white"  }
     return (
         <nav className="bg-gray-800 text-white p-4">
             <ul className="flex justify-center items-center gap-8">
-                <li className="hover:bg-gray-600 cursor-pointer">Home</li>
-                <li className="hover:bg-gray-600 cursor-pointer">About</li>
-                <li className="hover:bg-gray-600 cursor-pointer">Contact</li>
+              <li className="hover:bg-gray-600 cursor-pointer">
+                  <Link  className={isActive("/")} href="/"> Home </Link>
+               </li>
+
+               
+   <li className="hover:bg-gray-600 cursor-pointer">
+                  <Link className={isActive("/about")}href="/about"> About </Link>
+               </li>
+   <li className="hover:bg-gray-600 cursor-pointer">
+                  <Link className={isActive("/contact")} href="/contact"> Contact </Link>
+               </li>
             </ul>
         </nav>
     )
