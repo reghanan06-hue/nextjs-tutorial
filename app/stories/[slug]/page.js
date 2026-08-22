f713d10c15091da917a82f34383d247c8d3de749
reghanan06-hue <reghanan06@gmail.com>
2026-08-22 16:25:50 +0100

    Add seachParams of page stories

--- app/stories/[slug]/page.js
+++ app/stories/[slug]/page.js
@@ -9,13 +9,19 @@ title :   slug,
 
 
 }
-export default  async function Storiespage({params}) {
+export default  async function Storiespage({params,searchParams}) {
   const parameters=await params;
   const slug = parameters.slug;
+  const {filter,lang} =await searchParams;
+  console.log(filter);
+    console.log(lang);
+
   return (
     <div>
       <h1> welcome page stories</h1>
       <p>{slug}</p>
+      <p>{filter}</p>
+       <p className="text-red-800 font-bold">{lang}</p>
     </div>
   )
 }
--- app/stories/page.js
+++ app/stories/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export const metadata={
     title : "Stories",
     description:"Page of scories"
@@ -7,6 +8,11 @@ export default function Storiespage() {
   return (
     <div>
       <h1> welcome page stories</h1>
+     <Link href="/stories/first-story?filter=javascript">First story</Link>
+     <Link href="/stories/first-story?filter=python">Second story</Link>
+     <Link href="/stories/first-story?filter=nextjs">third story</Link>
+
+
     </div>
   )
 }
